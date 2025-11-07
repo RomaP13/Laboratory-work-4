f6db1e5396555c8e1ec7b561b186d97a5735291f
Roman Pomazan <romapomazan1310@gmail.com>
2025-11-07 09:46:25 +0200

    add programcode

---
+++ ProgramCode.js
@@ -0,0 +1,7 @@
+// Цей файл не пройде перевірку лінтером
+var appName = "InfoSafeSportRespect"; // Помилка: 'var' не рекомендується, краще 'const' або 'let'
+
+console.log(appName);
+
+// Помилка: зайва крапка з комою
+;;
